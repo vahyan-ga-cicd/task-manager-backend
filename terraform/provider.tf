f15c06efd46df2cdd6a1aabac2723bdcb4836534
Dipayan Dey <deydipayan2006@gmail.com>
2026-03-24 18:17:36 +0530

    feat: Initialize Terraform with AWS provider and S3 backend configuration.

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,7 +11,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "task-manager-state"
+    bucket = "vahyan-taskmanagement"
     key    = "backend/terraform.tfstate"
     region = "ap-south-1"
   }
